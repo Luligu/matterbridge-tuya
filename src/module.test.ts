5b1a0965b8bc1c95329cb77454afc9d8c2709ee5
Luligu <target.epithet-0h@icloud.com>
2025-11-13 14:09:36 +0100

    Initial commit (repository reset)

---
+++ src/module.test.ts
@@ -0,0 +1,98 @@
+const MATTER_PORT = 6000;
+const NAME = 'Platform';
+const HOMEDIR = path.join('jest', NAME);
+
+import path from 'node:path';
+
+import { Matterbridge, MatterbridgeEndpoint, PlatformConfig } from 'matterbridge';
+import { AnsiLogger } from 'matterbridge/logger';
+import { jest } from '@jest/globals';
+
+import initializePlugin, { Platform } from './module.js';
+import {
+  addMatterbridgePlatform,
+  createMatterbridgeEnvironment,
+  destroyMatterbridgeEnvironment,
+  log,
+  loggerInfoSpy,
+  matterbridge,
+  setupTest,
+  startMatterbridgeEnvironment,
+  stopMatterbridgeEnvironment,
+} from './utils/jestHelpers.js';
+
+setupTest('Platform');
+
+describe('TestPlatform', () => {
+  let platform: Platform;
+
+  const config: PlatformConfig = {
+    name: 'matterbridge-tuya',
+    type: 'DynamicPlatform',
+    version: '1.0.0',
+    debug: false,
+    unregisterOnShutdown: false,
+  };
+
+  beforeAll(async () => {
+    // Create Matterbridge environment
+    await createMatterbridgeEnvironment(NAME);
+    await startMatterbridgeEnvironment(MATTER_PORT);
+  });
+
+  beforeEach(() => {
+    // Reset the mock calls before each test
+    jest.clearAllMocks();
+  });
+
+  afterEach(async () => {
+    // Cleanup after each test
+  });
+
+  afterAll(async () => {
+    // Destroy Matterbridge environment
+    await stopMatterbridgeEnvironment();
+    await destroyMatterbridgeEnvironment();
+
+    // Restore all mocks
+    jest.restoreAllMocks();
+  });
+
+  it('should return an instance of TestPlatform', async () => {
+    platform = initializePlugin(matterbridge, log, config);
+    expect(platform).toBeInstanceOf(Platform);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Initializing platform:', config.name);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Finished initializing platform:', config.name);
+    await platform.onShutdown();
+  });
+
+  it('should throw error in load when version is not valid', () => {
+    matterbridge.matterbridgeVersion = '1.5.0';
+    expect(() => new Platform(matterbridge, log, config)).toThrow(
+      'This plugin requires Matterbridge version >= "3.3.0". Please update Matterbridge to the latest version in the frontend.',
+    );
+    matterbridge.matterbridgeVersion = '3.3.0';
+  });
+
+  it('should initialize platform with config name', () => {
+    platform = new Platform(matterbridge, log, config);
+    addMatterbridgePlatform(platform);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Initializing platform:', config.name);
+    expect(loggerInfoSpy).toHaveBeenCalledWith('Finished initializing platform:', config.name);
+  });
+
+  it('should call onStart with reason', async () => {
+    await platform.onStart('Test reason');
+    expect(loggerInfoSpy).toHaveBeenCalledWith('onStart called with reason:', 'Test reason');
+  });
+
+  it('should call onConfigure', async () => {
+    await platform.onConfigure();
+    expect(loggerInfoSpy).toHaveBeenCalledWith('onConfigure called');
+  });
+
+  it('should call onShutdown with reason', async () => {
+    await platform.onShutdown('Test reason');
+    expect(loggerInfoSpy).toHaveBeenCalledWith('onShutdown called with reason:', 'Test reason');
+  });
+});
---
+++ src/module.ts
@@ -0,0 +1,45 @@
+import { MatterbridgeDynamicPlatform, PlatformConfig, PlatformMatterbridge } from 'matterbridge';
+import { AnsiLogger } from 'matterbridge/logger';
+
+/**
+ * This is the standard interface for Matterbridge plugins.
+ * Each plugin should export a default function that follows this signature.
+ *
+ * @param {PlatformMatterbridge} matterbridge - An instance of MatterBridge. This is the main interface for interacting with the MatterBridge system.
+ * @param {AnsiLogger} log - An instance of AnsiLogger. This is used for logging messages in a format that can be displayed with ANSI color codes.
+ * @param {PlatformConfig} config - The platform configuration.
+ * @returns {Platform} - An instance of the SomfyTahomaPlatform. This is the main interface for interacting with the Somfy Tahoma system.
+ */
+export default function initializePlugin(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig): Platform {
+  return new Platform(matterbridge, log, config);
+}
+
+export class Platform extends MatterbridgeDynamicPlatform {
+  constructor(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig) {
+    super(matterbridge, log, config);
+
+    // Verify that Matterbridge is the correct version
+    if (this.verifyMatterbridgeVersion === undefined || typeof this.verifyMatterbridgeVersion !== 'function' || !this.verifyMatterbridgeVersion('3.3.0')) {
+      throw new Error(`This plugin requires Matterbridge version >= "3.3.0". Please update Matterbridge to the latest version in the frontend.`);
+    }
+
+    this.log.info('Initializing platform:', this.config.name);
+
+    this.log.info('Finished initializing platform:', this.config.name);
+  }
+
+  override async onStart(reason?: string): Promise<void> {
+    this.log.info('onStart called with reason:', reason ?? 'none');
+  }
+
+  override async onConfigure(): Promise<void> {
+    await super.onConfigure();
+    this.log.info('onConfigure called');
+  }
+
+  override async onShutdown(reason?: string): Promise<void> {
+    await super.onShutdown(reason);
+    this.log.info('onShutdown called with reason:', reason ?? 'none');
+    if (this.config.unregisterOnShutdown === true) await this.unregisterAllDevices();
+  }
+}
