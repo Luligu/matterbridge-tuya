741e762af7aba896878aab753f564f1ce1251715
Luligu <target.epithet-0h@icloud.com>
2026-06-07 16:08:06 +0100

    Bump to experimental

--- src/module.ts
+++ src/module.ts
@@ -1,5 +1,6 @@
-import { MatterbridgeDynamicPlatform, PlatformConfig, PlatformMatterbridge } from 'matterbridge';
-import { AnsiLogger } from 'matterbridge/logger';
+import { MatterbridgeDynamicPlatform } from 'matterbridge';
+import type { PlatformConfig, PlatformMatterbridge } from 'matterbridge';
+import { type AnsiLogger } from 'matterbridge/logger';
 
 /**
  * This is the standard interface for Matterbridge plugins.
@@ -8,29 +9,29 @@ import { AnsiLogger } from 'matterbridge/logger';
  * @param {PlatformMatterbridge} matterbridge - An instance of MatterBridge. This is the main interface for interacting with the MatterBridge system.
  * @param {AnsiLogger} log - An instance of AnsiLogger. This is used for logging messages in a format that can be displayed with ANSI color codes.
  * @param {PlatformConfig} config - The platform configuration.
- * @returns {Platform} - An instance of the SomfyTahomaPlatform. This is the main interface for interacting with the Somfy Tahoma system.
+ * @returns {TuyaPlatform} - An instance of the TuyaPlatform. This is the main interface for interacting with the Tuya system.
  */
-export default function initializePlugin(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig): Platform {
-  return new Platform(matterbridge, log, config);
+export default function initializePlugin(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig): TuyaPlatform {
+  return new TuyaPlatform(matterbridge, log, config);
 }
 
-export class Platform extends MatterbridgeDynamicPlatform {
+export class TuyaPlatform extends MatterbridgeDynamicPlatform {
   constructor(matterbridge: PlatformMatterbridge, log: AnsiLogger, config: PlatformConfig) {
     super(matterbridge, log, config);
 
     // Verify that Matterbridge is the correct version
-    if (typeof this.verifyMatterbridgeVersion !== 'function' || !this.verifyMatterbridgeVersion('3.5.0')) {
-      throw new Error(`This plugin requires Matterbridge version >= "3.5.0". Please update Matterbridge to the latest version in the frontend.`);
+    if (typeof this.verifyMatterbridgeVersion !== 'function' || !this.verifyMatterbridgeVersion('3.8.0')) {
+      throw new Error(`This plugin requires Matterbridge version >= "3.8.0". Please update Matterbridge to the latest version in the frontend.`);
     }
 
-    this.log.info('Initializing platform:', this.config.name);
+    this.log.info(`Initializing platform: ${this.config.name}`);
 
-    this.log.info('Finished initializing platform:', this.config.name);
+    this.log.info(`Finished initializing platform: ${this.config.name}`);
   }
 
   // eslint-disable-next-line @typescript-eslint/require-await
   override async onStart(reason?: string): Promise<void> {
-    this.log.info('onStart called with reason:', reason ?? 'none');
+    this.log.info(`onStart called with reason: ${reason ?? 'No reason provided'}`);
   }
 
   override async onConfigure(): Promise<void> {
@@ -40,7 +41,7 @@ export class Platform extends MatterbridgeDynamicPlatform {
 
   override async onShutdown(reason?: string): Promise<void> {
     await super.onShutdown(reason);
-    this.log.info('onShutdown called with reason:', reason ?? 'none');
-    if (this.config.unregisterOnShutdown === true) await this.unregisterAllDevices();
+    this.log.info(`onShutdown called with reason: ${reason ?? 'No reason provided'}`);
+    if (this.config.unregisterOnShutdown) await this.unregisterAllDevices();
   }
 }
